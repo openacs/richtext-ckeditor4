c00b5a67a3e79e533abc3bcb1063ca0435f2b14e
Gustaf Neumann <gustaf.neumann@wu-wien.ac.at>
2023-07-06 08:31:10 +0000

    bump version number of CKEditor to 4.22.1
    CKEditor 4 was launched in 2012 and reached its End of Life (EOL) on June 30, 2023.
    
    A special edition, CKEditor 4 LTS ("Long Term Support"), is available under commercial terms ("Extended Support Model") for anyone looking to extend the coverage of security updates and critical bug fixes.
    
    With CKEditor 4 LTS, security updates and critical bug fixes are guaranteed until December 2026.
    
    Details: https://github.com/ckeditor/ckeditor4-releases

--- tcl/richtext-procs.tcl
+++ tcl/richtext-procs.tcl
@@ -24,7 +24,7 @@ namespace eval ::richtext::ckeditor4 {
     # config file:
     #
     # ns_section ns/server/${server}/acs/richtext-ckeditor4
-    #        ns_param CKEditorVersion   4.20.1
+    #        ns_param CKEditorVersion   4.22.1
     #        ns_param CKEditorPackage   full
     #        ns_param CKFinderURL       /acs-content-repository/ckfinder
     #        ns_param StandardPlugins   uploadimage
@@ -32,7 +32,7 @@ namespace eval ::richtext::ckeditor4 {
     set ::richtext::ckeditor4::version [parameter::get \
                                             -package_id $package_id \
                                             -parameter CKEditorVersion \
-                                            -default 4.20.2]
+                                            -default 4.22.1]
 
     set ::richtext::ckeditor4::ckfinder_url [parameter::get \
                                                  -package_id $package_id \
