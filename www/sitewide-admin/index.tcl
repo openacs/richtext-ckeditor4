5c3f22aad04969cea9eba1a31f782f1574418844
Gustaf Neumann <gustaf.neumann@wu-wien.ac.at>
2024-07-24 16:31:45 +0000

    Improved consistency of resource-info for javascript libraries, added vulnarability info
    Tested the following cases:
    
    download check
    - bootstrap
    - bootstrap-icons
    - bootstrap-treeview
    - ckeditor4
    - font-awesome
    - highcharts
    - cookieconsent2 3.0.3
    - cookieconsent2 3.1.1
    
    CDN check
    - bootstrap
    - bootstrap-icons
    - bootstrap-treeview
    - font-awesome
    - highcharts
    - cookieconsent2 3.0.3
    - cookieconsent2 3.1.1

--- www/sitewide-admin/index.tcl
+++ www/sitewide-admin/index.tcl
@@ -12,7 +12,7 @@ if {$ck_package eq ""} {
     set ck_package $::richtext::ckeditor4::ck_package
 }
 set resource_info [::richtext::ckeditor4::resource_info -ck_package $ck_package]
-set download_url download?ck_package=$ck_package
+set download_url [ad_conn url]/download?ck_package=$ck_package
 
 set title "[dict get $resource_info resourceName] - Sitewide Admin"
 set context [list $title]
